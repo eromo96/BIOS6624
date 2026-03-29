e956db30d81139f706d5d87d900ee6e6af9c34cb
eromo96 <eduardoromo91596@gmail.com>
2026-03-29 13:16:49 -0600

    Made some edits in preliminary code to include vif calc. Also, created Power code which does the power calculation for each Aim. (I had been working on this for a few days now but just forgot to push).

---
+++ Project 2/Code/Power-SS.R
@@ -0,0 +1,1 @@
+# Power/Sample Size code
--- Project 2/Code/Preliminary.R
+++ Project 2/Code/Preliminary.R
@@ -1,7 +1,7 @@
 #Code to review/investigate preliminary data
 
 library(tidyverse)
-
+library(car)
 #read in preliminary data
 prelim_data <- read_csv("./Project 2/Data/PrelimData.csv")
 
@@ -11,5 +11,10 @@ summary(prelim_data)
 #run mlr models between predictors and each outcome separately to get R^2
 cort_mod <- lm(CORT_CNG3 ~ IL_6 + MCP_1, data = prelim_data)
 summary(cort_mod)
+vif(cort_mod)
 cvlt_mod <- lm(CVLT_CNG3 ~ IL_6 + MCP_1, data = prelim_data)
 summary(cvlt_mod)
+vif(cvlt_mod)
+# Quick correlation matrix
+cor_mat <- cor(prelim_data, use = "complete.obs")
+print(round(cor_mat, 3))
